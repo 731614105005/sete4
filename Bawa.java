b1a9661b8548506123455b8977e6f5dba143d2d6
731614105005 <bawadharani96@gmail.com>
2017-07-07 15:37:18 +0530

    Create Bawa.java

---
+++ Bawa.java
@@ -0,0 +1,15 @@
+# sete4
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Bawa
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    String s="10";
+    int m=Integer.parseInt(s);
+    System.out.println(""+m);
+    }
+    }
+ 
